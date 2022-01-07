8b8352a503a5cd8101588214045e42785ec88011
Raquel Hill <raquelturing@gmail.com>
2022-01-06 22:24:50 -0700

    Add and style home button

--- src/componenets/NavBar/NavBar.js
+++ src/componenets/NavBar/NavBar.js
@@ -6,16 +6,18 @@ const Nav = () => {
       <h1>
         News <span>Flash</span>
       </h1>
-      <div className="nav-links">
-        <p className="news-sections">Home</p>
-        <p className="news-sections">Arts</p>
-        <p className="news-sections">Science</p>
-        <p className="news-sections">US</p>
-        <p className="news-sections">World</p>
-        <p className="news-sections">Business</p>
-        <p className="news-sections">Opinion</p>
-        <p className="news-sections">Politics</p>
-        <p className="news-sections">Sports</p>
+      <div className="link-styling">
+        <div className="nav-links">
+          <p className="news-sections">Arts</p>
+          <p className="news-sections">Science</p>
+          <p className="news-sections">US</p>
+          <p className="news-sections">World</p>
+          <p className="news-sections">Business</p>
+          <p className="news-sections">Opinion</p>
+          <p className="news-sections">Politics</p>
+          <p className="news-sections">Sports</p>
+        </div>
+        <button className="home-btn">HOME</button>
       </div>
     </nav>
   );
